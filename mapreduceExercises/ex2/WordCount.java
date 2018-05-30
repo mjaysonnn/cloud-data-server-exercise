b082bab8748de0bba8d5fe542684f161083e0062
SON MYUNG JUN <smj8612@kookmin.ac.kr>
2018-05-30 23:10:49 +0900

    add the solution for ex2

--- mapreduceExercises/ex2/WordCount.java
+++ mapreduceExercises/ex2/WordCount.java
@@ -1,48 +1,28 @@
+package kr.ac.kookmin.cs.bigdata;
+
 import java.io.IOException;
-import java.util.*;
+
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 public class WordCount {
-    public static class Map extends
-            Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
-        public void map(LongWritable key, Text value, Context context)
-                throws IOException, InterruptedException {
-            String line = value.toString();
-            String[] lineArray = line.split(",");
-            String lastWord = lineArray[lineArray.length - 1];
-            String[] genreArray = lastWord.split("\\|");
-            for(String genre_value : genreArray){
-                word.set(genre_value);
-                context.write(word, one);
-            }
-        }
-    }
-
-    public static class Reduce extends
-            Reducer<Text, IntWritable, Text, IntWritable> {
-
-        public void reduce(Text key, Iterable<IntWritable> values,
-                Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            context.write(key, new IntWritable(sum));
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
 
-        Job job = new Job(conf, "wordcount");
+        // Create a new job
+        Job job = Job.getInstance(conf, "wordcount");
+
+        // Use the WordCount.class file to point to the job jar
         job.setJarByClass(WordCount.class);
 
         job.setOutputKeyClass(Text.class);
@@ -50,13 +30,52 @@ public class WordCount {
 
         job.setMapperClass(Map.class);
         job.setReducerClass(Reduce.class);
+        
+        job.setNumReduceTasks(1);
 
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
+        // Setting the input and output locations
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
+        // Submit the job and wait for it's completion
         job.waitForCompletion(true);
     }
+
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] fields = line.split(",");
+            String lastField = fields[fields.length - 1];
+            String[] genres = lastField.split("\\|");
+
+            for (String genre : genres) {
+                if (genre.equals("Film-Noir")) {
+                    word.set(genre);
+                    context.write(word, one);
+                }
+            }
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+
+            // Sum all the occureeences of the word (key)
+            for (IntWritable value : values) {
+                sum += value.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
 }
+
+
